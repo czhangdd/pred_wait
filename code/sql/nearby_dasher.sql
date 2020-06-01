543dc4b91fba72412b676460054aa2da9df3f4b2
Chi Zhang <chi.zhang@doordash.com>
2020-06-01 10:42:42 -0700

    use init_shift_candidates table instead of all_shifts

--- code/sql/nearby_dasher.sql
+++ code/sql/nearby_dasher.sql
@@ -1,3 +1,5 @@
+set exp_start = '2020-03-16';
+set exp_end = '2020-04-15';
 
 ------------------------------------------
 -------------- geo table ----------------
@@ -5,24 +7,24 @@
 drop table if exists four_weeks_geo_candidate_shifts;
 create table four_weeks_geo_candidate_shifts as (
 select 
-    all_shifts.EVENT_CREATED_AT as all_shift_event_created_at,
+    init_shift_candidates.EVENT_CREATED_AT as init_shift_candidates_event_created_at,
     candidate_shifts.EVENT_CREATED_AT as candidate_event_created_at,
     SHIFT_LAT,
     SHIFT_LNG,
-    case 'false' when is_idle_shift then 0 else 1 end as IS_IDLE_SHIFT,
-    DASHER_ID,
-    all_shifts.SP_ID,  
-    all_shifts.ASSIGNMENT_RUN_ID,
-    all_shifts.SHIFT_ID,
+    case when IS_SHIFT_BUSY='false' then 0
+         when IS_SHIFT_BUSY='true' then 1
+         else -1 end as IS_SHIFT_BUSY,
+    init_shift_candidates.SP_ID,
+    init_shift_candidates.ASSIGNMENT_RUN_ID,
+    init_shift_candidates.SHIFT_ID,
     candidate_shifts.delivery_id
   from INGEST_DEEPRED_SERVER_EVENTS_PROD.EVENT_DEEP_RED_OPTIMIZER_CANDIDATE_ASSIGNMENTS candidate_shifts
-  join ingest_deepred_server_events_prod.event_deep_red_all_shifts all_shifts
-    on candidate_shifts.ASSIGNMENT_RUN_ID = all_shifts.ASSIGNMENT_RUN_ID and
-       candidate_shifts.SHIFT_ID = all_shifts.SHIFT_ID
-  where candidate_shifts.EVENT_CREATED_AT between '2020-03-16' and '2020-03-17' and  --0415
-        all_shifts.EVENT_CREATED_AT between '2020-03-16' and '2020-03-17'
+  join Ingest_deepred_server_events_prod.event_deep_red_initial_shift_candidates init_shift_candidates
+    on candidate_shifts.ASSIGNMENT_RUN_ID = init_shift_candidates.ASSIGNMENT_RUN_ID and
+       candidate_shifts.SHIFT_ID = init_shift_candidates.SHIFT_ID
+  where candidate_shifts.EVENT_CREATED_AT between $exp_start and $exp_end and  --0415
+        init_shift_candidates.EVENT_CREATED_AT between $exp_start and $exp_end
 );
-
 
 ------------------------------------------
 -------------- wait table ----------------
@@ -35,7 +37,7 @@ select
     cds.delivery_id, 
     min(ORIGINAL_TIMESTAMP) as first_assignment_time_utc
 from segment_events.server_events_production.deep_red_munkres_considered_delivery_stats cds
-    where to_timestamp_ntz(cds.TIMESTAMP) between '2020-03-16' and '2020-04-15'
+    where to_timestamp_ntz(cds.TIMESTAMP) between $exp_start and $exp_end
 group by delivery_id
 ),
 
@@ -77,13 +79,13 @@ select
     madd.EXT_POINT_LAT, 
     madd.EXT_POINT_LONG,
     timediff(s, dd.dasher_at_store_time, cds_real_time_feat.order_ready_time_utc) as wait_before_ready_time
-    -- timediff(s, cds_real_time_feat.order_ready_time_utc, dd.ACTUAL_PICKUP_TIME) as late_after_ready_time
 from PUBLIC.DIMENSION_DELIVERIES dd
 JOIN cds_real_time_feat
     ON dd.delivery_id = cds_real_time_feat.delivery_id
+  -- join with maindb_address to get store lat and long
 join maindb_address madd
     on dd.pickup_address_id = madd.id
-  
+
 WHERE dd.IS_ASAP = True
   AND dd.business_id not in (185179, 12284, 12970, 10171, 9144, 8159, 206181, 1855, 115, 11671, 1798, 25117, 2764, 53152, 491, 953, 13681, 1431, 15444, 4477, 139197, 5579, 58164,42492, 5235, 1673, 3612, 3673, 8312, 47852, 12860, 1133, 6731, 3720, 4815, 7376, 1125, 12264)
   AND dd.is_filtered = TRUE
@@ -122,21 +124,22 @@ select * from wait_avg_store_assign sample(20)
 --------------- wait_geo table --------------
 ---------------------------------------------
 drop table if exists wait_geo_table;
-create table wait_geo_table as (-- get distance from one shift to one store, for each assignment_run_id
+create table wait_geo_table as (
+-- get distance from one shift to one store, for each assignment_run_id
 with delivery_distance as (
-select 
+select
        geo.delivery_id,
        geo.assignment_run_id,
        geo.shift_id,       
-       geo.is_idle_shift,
+       geo.IS_SHIFT_BUSY,
        geo.shift_lat,
        geo.shift_lng,
        wait_table.EXT_POINT_LAT,
-       wait_table.ext_point_long,
+       wait_table.EXT_POINT_LONG,
        haversine(shift_lat, SHIFT_LNG, EXT_POINT_LAT, EXT_POINT_LONG) as distance,
        case when distance<2 then 1 else 0 end as is_nearby,
-       case when (is_nearby=1 and is_idle_shift=1) then 'nearby_idle' 
-            when (is_nearby=1 and is_idle_shift=0) then 'nearby_busy' 
+       case when (is_nearby=1 and IS_SHIFT_BUSY=0) then 'nearby_idle' 
+            when (is_nearby=1 and IS_SHIFT_BUSY=1) then 'nearby_busy' 
             else 'faraway' end as dasher_status
 
 from  wait_table
@@ -147,7 +150,7 @@ join four_weeks_geo_candidate_shifts geo
 ,
 
 delivery_num_nearby as (
-select 
+select
     delivery_id,
     assignment_run_id,
     sum(case when dasher_status = 'nearby_idle' then 1 else 0 end) as num_nearby_idle,
@@ -164,4 +167,5 @@ from wait_table
 join delivery_num_nearby dnn
   on dnn.delivery_id = wait_table.delivery_id and
      dnn.assignment_run_id = wait_table.assignment_run_id
-)    
+limit 10
+)

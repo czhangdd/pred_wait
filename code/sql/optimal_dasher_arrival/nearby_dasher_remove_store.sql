1d7cd8346f10c391de005d164e14be7867eaf77d
Chi Zhang <chi.zhang@doordash.com>
2020-08-19 16:45:07 -0700

    remove redundent file

--- code/sql/optimal_dasher_arrival/nearby_dasher_remove_store.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists CHIZHANG.WAIT_GEO_TABLE_REMOVE_STORE;
-create table CHIZHANG.WAIT_GEO_TABLE_REMOVE_STORE as(
-SELECT *
-FROM CHIZHANG.WAIT_GEO_TABLE
-WHERE store_id NOT IN
-    (SELECT store_id 
-     FROM proddb.yihantan.pseudo_dasher_store_ids_v1)
-);
-grant select on CHIZHANG.WAIT_GEO_TABLE_REMOVE_STORE to role read_only_users;

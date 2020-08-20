3b4156288b4c65177518d839e7e23eb430981ac8
Chi Zhang <chi.zhang@doordash.com>
2020-08-19 23:36:04 -0700

    update instant assign table. now have much more stores

--- code/sql/optimal_dasher_arrival/pred_wait_train_data_remove_store.sql
+++ code/sql/optimal_dasher_arrival/pred_wait_train_data_remove_store.sql
@@ -3,6 +3,7 @@ SELECT *
 FROM CHIZHANG.pred_wait_final_0805
 WHERE store_id NOT IN
     (SELECT store_id 
-     FROM proddb.yihantan.pseudo_dasher_store_ids_v1)
+    --  FROM proddb.yihantan.pseudo_dasher_store_ids_v1
+     FROM proddb.yihantan.instant_assign_08072020)
 );
 grant select on CHIZHANG.pred_wait_final_0805_remove_store to role read_only_users;

a30864c4cc6ffba3880b97e40156a4247bff2ef0
Chi Zhang <chi.zhang@doordash.com>
2020-09-10 12:32:44 -0700

    merge data generation sql for shipped exp

---
+++ code/sql/v2_mx_pickuplogistics_only/pred_wait_train_data_remove_store.sql
@@ -0,0 +1,9 @@
+create or replace table CHIZHANG.pred_wait_final_0805_remove_store as(
+SELECT *
+FROM CHIZHANG.pred_wait_final_0805
+WHERE store_id NOT IN
+    (SELECT store_id 
+    --  FROM proddb.yihantan.pseudo_dasher_store_ids_v1
+     FROM proddb.yihantan.instant_assign_08072020)
+);
+grant select on CHIZHANG.pred_wait_final_0805_remove_store to role read_only_users;
